f8424b7fff6d3e47bb63a8a1b861f66625aaeb85
j-s-andersson <andersson@disi.unitn.it>
2014-02-20 15:34:46 +0100

    Minor update

--- src/main/java/src/eu/trentorise/opendata/columnrecognizers/RowTable.java
+++ src/main/java/src/eu/trentorise/opendata/columnrecognizers/RowTable.java
@@ -6,11 +6,8 @@ import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 
@@ -192,78 +189,6 @@ public class RowTable implements Table {
 		
 		return column;
 	}
-//		
-//	/**
-//	 * Extracts a list of the words occurring in the table, preserving their
-//	 * order.
-//	 * 
-//	 * @return	The word list
-//	 */
-//	public List<String> extractWords() {
-//		List<String> words = new ArrayList<String>();
-//		Iterator<String> it = rows.iterator();
-//		while (it.hasNext()) {
-//			String row = it.next();
-//			String[] rowWords = row.split("\\W+");
-//			for (int i = 0; i < rowWords.length; i++) {
-//				String word = CRStringUtils.normalize(rowWords[i]);
-//				if (!word.isEmpty()) {
-//					words.add(word);
-//				}
-//			}
-//		}
-//		return words;
-//	}
-//	
-//	/**
-//	 * Gets the set of terms in the table
-//	 * 
-//	 * @return	The set of terms
-//	 */
-//	public Set<String> extractWordSet() {
-//		// Suppress the shortest words
-//		final int MINIMAL_WORD_LENGTH = 3;
-//
-//		Set<String> words = new HashSet<String>();
-//		Iterator<String> it = rows.iterator();
-//		while (it.hasNext()) {
-//			String row = it.next();
-//			String[] rowWords = row.split("\\W+");
-//			for (int i = 0; i < rowWords.length; i++) {
-//				String word = CRStringUtils.normalize(rowWords[i]);
-//				if (word.length() >= MINIMAL_WORD_LENGTH) {
-//					words.add(word);
-//				}
-//			}
-//		}
-//		return words;
-//	}
-//	
-//	/**
-//	 * Computes the frequencies of words in the table.
-//	 * 
-//	 * @return	The word frequencies
-//	 */
-//	public Map<String, Integer> computeWordFrequencies () {
-//		// Suppress the shortest words
-//		final int MINIMAL_WORD_LENGTH = 3;
-//		
-//		Map<String, Integer> frequencies = new HashMap<String, Integer>();
-//		List<String> words = extractWords();
-//		Iterator<String> it = words.iterator();
-//		while (it.hasNext()) {
-//			String word = it.next();
-//			if (word.length() >= MINIMAL_WORD_LENGTH) {
-//				if (frequencies.containsKey(word)) {
-//					frequencies.put(word, frequencies.get(word) + 1);
-//				} else {
-//					frequencies.put(word, 1);
-//				}
-//			}
-//		}
-//		return frequencies;
-//	}
-//
 	
 	/**
 	 * Loads rows from a CSV file and appends them to the table.
